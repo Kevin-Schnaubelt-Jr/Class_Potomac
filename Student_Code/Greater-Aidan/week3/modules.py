9c0f5f104af43dabcd0c2ece95a398a4a843d40d
aidan <aidan.dreier@gmail.com>
2023-09-22 17:28:38 -0400

    num_to_phrase__turn_in

---
+++ Student_Code/Greater-Aidan/week3/modules.py
@@ -0,0 +1,11 @@
+result = 23/10
+#print(result)
+#answer is 2.3
+result = 23//10
+#print(result)
+#now it equals 2
+#floor division
+result = 23%10
+#print(result)
+#opposite of floor division
+#answer is 3
